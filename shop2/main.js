40b0e911f1a4b5454baf58ec88ef3854772d7c62
Jesús Álvarez <khisus19@gmail.com>
2022-10-13 23:01:30 -0500

    Refactoring the project to include JS and unifying the html in one file

---
+++ shop2/main.js
@@ -0,0 +1,26 @@
+const menuEmail = document.querySelector('.navbar-email')
+const desktopMenu = document.querySelector('.desktop-menu')
+const hamburguerIcon = document.querySelector('.nav-hamburguer')
+const mobileMenu = document.querySelector('.mobile-menu')
+const shopingCartIcon = document.querySelector('.shoping-cart_container')
+const shopingCartMenu = document.querySelector('.my-order')
+const shopingCartLeftArrow = document.querySelector('.left-arrow')
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburguerIcon.addEventListener('click', toggleMobileMenu);
+shopingCartIcon.addEventListener('click', toggleShopingCartMenu)
+shopingCartLeftArrow.addEventListener('click', toggleShopingCartMenu)
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle('inactive')
+  shopingCartMenu.classList.add('inactive')
+}
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive')
+  shopingCartMenu.classList.add('inactive')
+}
+function toggleShopingCartMenu() {
+  shopingCartMenu.classList.toggle('inactive')
+  desktopMenu.classList.add('inactive')
+  mobileMenu.classList.add('inactive')
+}
